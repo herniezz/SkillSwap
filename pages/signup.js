75fdb13fb83a81347e1ced92214bc6dced6f6fd4
Myog3 <dkozakszkola@gmail.com>
2024-12-03 21:28:03 +0100

    Redirect to quiz_page after sign up

--- pages/signup.js
+++ pages/signup.js
@@ -2,6 +2,6 @@ import { SignUp} from '@clerk/nextjs'
 
 export default function Login() {
   return (
-   <SignUp routing='hash'/>
+   <SignUp routing='hash' forceRedirectUrl='/quiz_page'/>
   );
 }
