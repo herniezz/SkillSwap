8446d379e3a5cdb8d2675dd4759ba097c0eb2f07
herniezz <92106489+herniezz@users.noreply.github.com>
2025-01-22 10:52:03 +0100

    update on databases

--- pages/signup.js
+++ pages/signup.js
@@ -1,7 +1,33 @@
-import { SignUp} from '@clerk/nextjs'
+// pages/quiz_page.js
+import { useEffect } from 'react';
+import { useUser } from '@clerk/nextjs';
 
-export default function Login() {
-  return ( <center>
-   <SignUp routing='hash' forceRedirectUrl='/quiz_page'/> </center>
-  );
+export default function QuizPage() {
+    const { isLoaded, user } = useUser();
+
+    useEffect(() => {
+        if (isLoaded && user) {
+            // Call the register endpoint
+            fetch('/api/users/register', {
+                method: 'POST',
+                credentials: 'include', // Ensures Clerk auth cookies are sent
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                // Body not needed if server extracts user info from Clerk session
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('User registration response:', data);
+                })
+                .catch(err => console.error('Error registering user:', err));
+        }
+    }, [isLoaded, user]);
+
+    return (
+        <div>
+            <h1>Welcome to the Quiz Page!</h1>
+            {/* ... rest of your quiz page content ... */}
+        </div>
+    );
 }
